85452a98840da15979ce7624b80a7cad96d0b9d2
LuckCky <www.aivanov@gmail.com>
2017-04-16 23:52:53 +0300

    debugging

--- eternal_fb_token.py
+++ eternal_fb_token.py
@@ -3,9 +3,9 @@
 # Facebook SDK
 import facebook
 
-short_token = 'EAAJbABiT2nMBANjPVmWDwDfJR8ZBZAhxyKpiwbbxtgcjx1GdZAobcWjDMEh7krP7qNn0ZCR6hAwZApcyuCsolQf5jbnZA1F0h6LlVWz91d0kXsPPZAaQoPyzah4LvPUDfZBAciaRGJYFkXepxzXM1wZBuWuXlDSNweuJypsSJcgHz8iZB7ZA04PWcDdwaygrgmDLUUZD'
-client_id = '663005923891827'
-client_secret = '006135fcf69bf5cd058f99d51d915056'
+short_token = ''
+client_id = ''
+client_secret = ''
 
 # Init graphAPI with short-lived token
 graph = facebook.GraphAPI(short_token)
