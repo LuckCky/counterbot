c20e7fe420748d787bf16ccff3a09dd3f74b0347
LuckCky <www.aivanov@gmail.com>
2017-04-16 12:53:43 +0300

    first ver echo bot

---
+++ bot_webhook.py
@@ -0,0 +1,67 @@
+import cherrypy
+import os
+import random
+import time
+
+import telebot
+
+import conf
+
+token = os.environ.get('BOT_TOKEN')
+CHAT_ID = os.environ.get('CHAT_ID')
+
+WEBHOOK_PORT = int(os.environ.get('PORT', '5000'))
+WEBHOOK_LISTEN = conf.webhook_listen
+
+WEBHOOK_URL_BASE = conf.post_url
+WEBHOOK_URL_PATH = "/{}/".format(token)
+
+bot = telebot.TeleBot(token)
+
+
+def choose_message(messages, end_random=50):
+    num = random.randint(0, end_random)
+    if num < len(messages):
+        return messages[num]
+    else:
+        return None
+
+
+@bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video',
+                                    'voice', 'location', 'contact'])
+def echo_all(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    time.sleep(3)
+    bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+
+    cherrypy.config.update({
+        'engine.autoreload.on': False,
+        'server.socket_host': WEBHOOK_LISTEN,
+        'server.socket_port': WEBHOOK_PORT,
+    })
+
+    # RUN SERVER, RUN!
+    cherrypy.tree.mount(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+    # bot.polling()
---
+++ conf.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Your own chat id. Ask https://telegram.me/my_id_bot to tell you yours
+my_id = ''
+
+# cherrypy server params
+webhook_listen = '0.0.0.0'
+
+post_url = 'https://guarded-tundra-24703.herokuapp.com/'
+
+# phrases for bot
