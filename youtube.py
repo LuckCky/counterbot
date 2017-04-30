3e15d9b6c4a1db3e9dd3e2c7a292d189ae7f7e83
LuckCky <www.aivanov@gmail.com>
2017-04-30 20:47:11 +0300

    debugging

--- youtube.py
+++ youtube.py
@@ -11,11 +11,12 @@ def get_youtube_fans(group_name):
         group_url = youtube_groups[group_name]
     except KeyError:
         return 'В моём списке нет такой группы. Проверьте настройки или написание'
-    url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={0}&key={1}'.\
+    url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={0}&key={1}'. \
         format(group_url, api_key)
     r = requests.get(url)
     try:
         fans = r.json()['items'][0]['statistics']['subscriberCount']
     except KeyError:
         return 'Youtube вернул странный ответ, который я не могу распарсить: {}'.format(r.content)
+    print(type(fans))
     return fans
