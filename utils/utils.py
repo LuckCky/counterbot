631cf68bb0753812457347cc8b34916cb221f9fc
LuckCky <www.aivanov@gmail.com>
2017-06-20 22:35:18 +0300

    debugging

--- utils/utils.py
+++ utils/utils.py
@@ -14,9 +14,10 @@ def report_needed(message):
 
 
 def message_parser(message):
-    alias = message.split(' ')[1].strip()
-    if len(message.split(' ')) >= 3:
-        network = message.split(' ')[2].strip()
+    message_split = message.split(' ')
+    alias = message_split[1].strip()
+    if len(message_split) >= 3:
+        network = message_split[-1].strip()
     else:
         network = None
     return alias, network
