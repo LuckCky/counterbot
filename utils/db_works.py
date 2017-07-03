2cfd3fe181de84addb3e42ebbdeadfe9915e0fe9
LuckCky <www.aivanov@gmail.com>
2017-07-03 23:11:40 +0300

    debugging

--- utils/db_works.py
+++ utils/db_works.py
@@ -48,5 +48,5 @@ class DBWorks(object):
         return self.cursor.fetchall()
 
     def get_info_two_args(self, statement, args):
-        self.cursor.execute(statement, *args)
+        self.cursor.execute(statement, args)
         return self.cursor.fetchall()
