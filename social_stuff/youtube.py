a974589a762af4c96abf7f9e1633470329842255
LuckCky <www.aivanov@gmail.com>
2017-07-03 23:42:19 +0300

    debugging

--- social_stuff/youtube.py
+++ social_stuff/youtube.py
@@ -2,16 +2,10 @@ import os
 
 import requests
 
-from social_stuff.groups import youtube_groups
-
 api_key = os.environ.get('YOUTUBE_API_KEY')
 
 
-def get_youtube_fans(group_name):
-    try:
-        group_url = youtube_groups[group_name]
-    except KeyError:
-        return 'В моём списке нет такой группы. Проверьте настройки или написание'
+def get_youtube_fans(group_url):
     url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={0}&key={1}'. \
         format(group_url, api_key)
     r = requests.get(url)
