e1d7d686555b7bc976346ff0afa877963015d531
LuckCky <www.aivanov@gmail.com>
2017-07-31 23:32:45 +0300

    scheduler

--- scheduler.py
+++ scheduler.py
@@ -12,7 +12,7 @@ SCHEDULER.start()
 class Scheduler(object):
 
     def __init__(self):
-        self.hour = '12.00'
+        self.hour = '23.34'
 
     def add_get_all_fans_job(self):
         SCHEDULER.add_job(get_all_fans_job(), 'cron', hour=self.hour, misfire_grace_time=120)
