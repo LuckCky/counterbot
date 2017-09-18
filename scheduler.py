7e42f11ee9c73b04ab49ec92eab2139d52b37e88
LuckCky <www.aivanov@gmail.com>
2017-09-18 23:21:53 +0300

    scheduler testing

--- scheduler.py
+++ scheduler.py
@@ -12,11 +12,10 @@ SCHEDULER.start()
 class Scheduler(object):
 
     def __init__(self):
-        self.hour = 20
-        self.minute = 7
+        self.hour = 12
 
     def add_get_all_fans_job(self):
-        SCHEDULER.add_job(get_all_fans_job(), 'cron', hour=self.hour, minute=self.minute)
+        SCHEDULER.add_job(get_all_fans_job(), 'cron', hour=self.hour)
 
 if __name__ == "__main__":
     s = Scheduler()
