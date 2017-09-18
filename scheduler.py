af2e874c4de9cde0c80fd03259c64eb27162f340
LuckCky <www.aivanov@gmail.com>
2017-09-18 22:31:07 +0300

    scheduler testing

--- scheduler.py
+++ scheduler.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
 
 from scheduler_job import get_all_fans_job
 
 # initialize scheduler
-SCHEDULER = BackgroundScheduler()
+SCHEDULER = BlockingScheduler()
 SCHEDULER.start()
 
 
 class Scheduler(object):
 
     def __init__(self):
-        self.hour = 12
+        self.hour = 19
+        self.minute = 33
 
     def add_get_all_fans_job(self):
-        SCHEDULER.add_job(get_all_fans_job(), 'cron', hour=self.hour)
+        SCHEDULER.add_job(get_all_fans_job(), 'cron', hour=self.hour, minute=self.minute)
 
 if __name__ == "__main__":
     s = Scheduler()
