ab39ae02b71d90c760460fd9940ef97d594303b0
LuckCky <www.aivanov@gmail.com>
2017-09-24 15:30:32 +0300

    heroku example

--- clock.py
+++ clock.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
-from scheduler import Scheduler
+# from scheduler import Scheduler
+#
+# s = Scheduler()
+# s.add_get_all_fans_job()
+# print("I started!!!!!!!")
 
-s = Scheduler()
-s.add_get_all_fans_job()
-print("I started!!!!!!!")
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('This job is run every three minutes.')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+
+sched.start()
